e1e2d5b9af647a6c142bbc781162610c182bbfc6
Omar Tarek <omar.tarekabdelwahab@gmail.com>
2024-12-20 01:15:04 +0200

    Refactor Predictive Parser: Introduce GrammarReader class and GrammarReader.h

---
+++ Predictive_Parser/include/GrammerReader.h
@@ -0,0 +1,36 @@
+#ifndef GRAMMERREADER_H
+#define GRAMMERREADER_H
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#define LETF_TERM '<'
+#define RIGHT_TERM '>'
+
+class GrammarReader {
+public:
+    struct Production {
+        std::string lhs;
+        std::vector<std::string> rhs;
+    };
+
+    GrammarReader(std::string filepath);
+    std::vector<Production> getProductions();
+    void displayProductions();
+
+private:
+    int i;
+    std::ifstream file;
+    std::string cur_line;
+    std::vector<Production> productions;
+
+    void readGrammer();
+    bool is_special(char ch);
+    std::string next_token();
+    void remove_spaces();
+    std::string get_word();
+};
+
+#endif // GRAMMERREADER_H
