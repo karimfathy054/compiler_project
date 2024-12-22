596bd2ae0a42beb2250491efaac3643ceb18876e
Marshelino Maged <marshelinomaged@gmail.com>
2024-12-22 19:18:09 +0200

    first function implementation

--- Predictive_Parser/GrammarReader2.cpp
+++ Predictive_Parser/GrammarReader2.cpp
@@ -147,4 +147,15 @@ unordered_map<string, Symbol*> GrammarReader::getSymbols(){
 Symbol* GrammarReader::getStartSymbol(){
     return this->start_symbol;
 }
+// int main() {
+//     try {
+//         std::string filePath = "../../ll_grammar.txt"; // Replace with the path to your grammar file
+//         GrammarReader parser(filePath);
+//         parser.displayProductions();
+//     } catch (const std::exception& e) {
+//         std::cerr << "Error: " << e.what() << std::endl;
+//     }
+
+//     return 0;
+// }
 
