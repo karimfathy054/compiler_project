a3fbaa0e3635ad5ef4dff63885bb790d0386d3a5
Marshelino Maged <marshelinomaged@gmail.com>
2024-12-23 03:01:09 +0200

    follow function completed

--- Predictive_Parser/include/FirstFollowGen.h
+++ Predictive_Parser/include/FirstFollowGen.h
@@ -12,7 +12,7 @@
 
 class FirstFollowGen {
     public:
-        FirstFollowGen(std::vector<Production*> productions);
+        FirstFollowGen(std::vector<Production*> productions, Symbol* startSymbol);
         void getFirstSet();
         void getFollowSet();
         void displayFirst();
@@ -21,7 +21,9 @@ class FirstFollowGen {
         std::vector<std::unordered_set<Symbol*>> first;
         std::unordered_map<Symbol*, std::unordered_set<Symbol*>> follow;
         std::vector<Production*> productions;
-        std::unordered_set<Symbol*> computeFirstOfRhs(std::vector<Symbol*> rhs, int productionIndex);
+        Symbol* startSymbol;
+        std::unordered_set<Symbol*> computeFirstOfRhs(std::vector<Symbol*> rhs);
+        std::unordered_set<Symbol*> getFirstSetForSymbol(Symbol* symbol);
         void computeFirstSet();
         void computeFollowSet();
 };
